b3d39d6bfa27dc70887d322ae5137020e26c58ff
kihors <kihors@gmail.com>
2019-05-31 12:46:15 +0300

    made it more simply

--- src/homeworkSecond/TruthVerification.java
+++ src/homeworkSecond/TruthVerification.java
@@ -3,14 +3,10 @@ package homeworkSecond;
 public class TruthVerification {
 
     public static void main(String[] args) {
-        System.out.println(booleanExpression(true, false,
-                false, true));
+        System.out.println(booleanExpression(true, true, false, false));
     }
 
-    private static boolean booleanExpression(boolean firstArgument, boolean secondArgument,
-                                             boolean thirdArgument, boolean fourthArgument) {
-        return (firstArgument | secondArgument | thirdArgument | fourthArgument) &&
-                !(firstArgument & secondArgument & thirdArgument & fourthArgument) &&
-                !(firstArgument ^ secondArgument ^ thirdArgument ^ fourthArgument);
+    private static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
+        return (a ^ b) & (c ^ d) || (a ^ c) & (b ^ d);
     }
 }
