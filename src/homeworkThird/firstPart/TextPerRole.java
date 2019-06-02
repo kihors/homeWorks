b1bfa77d0f8c1aadddf021ff0af152c7c0dd9438
kihors <kihors@gmail.com>
2019-06-02 21:05:27 +0300

    third homework in package firstPart

---
+++ src/homeworkThird/firstPart/TextPerRole.java
@@ -0,0 +1,36 @@
+package homeworkThird.firstPart;
+
+public class TextPerRole {
+
+    public static void main(String[] args) {
+        String[] roles = {
+                "Городничий", "Аммос Федорович",
+                "Артемий Филиппович",
+                "Лука Лукич"};
+        String[] textLines = {
+                "Городничий: Я пригласил вас, господа, с тем, " +
+                        "чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+                "Аммос Федорович: Как ревизор?",
+                "Артемий Филиппович: Как ревизор?",
+                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+                "Аммос Федорович: Вот те на!",
+                "Артемий Филиппович: Вот не было заботы, так подай!",
+                "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
+        System.out.println(printTextPerRole(roles, textLines));
+    }
+
+    private static String printTextPerRole(String[] roles, String[] textLines) {
+        StringBuilder readyText = new StringBuilder();
+        for (String role : roles) {
+            readyText.append(role).append(":\n");
+            for (int i = 0; i < textLines.length; i++) {
+                if (textLines[i].startsWith(role + ":")) {
+                    readyText.append(i + 1).append(")").append(textLines[i].
+                            substring(role.length() + 1)).append("\n");
+                }
+            }
+            readyText.append("\n");
+        }
+        return readyText.toString();
+    }
+}
