e777771bc43c1bc3a898309fc4531ebf0d795653
kihors <kihors@gmail.com>
2019-06-14 09:01:47 +0300

    updated commit

--- src/homeworkTenth/MyHashMap.java
+++ src/homeworkTenth/MyHashMap.java
@@ -1,5 +1,60 @@
 package homeworkTenth;
 
-public class MyHashMap {
+import java.util.Map;
+import java.util.Objects;
 
+public class MyHashMap<K, V> {
+
+    private Entry<K, V>[] buckets;
+    private static final int INITIAL_CAPACITY = 1 << 4;
+    private int size = 0;
+
+    public void put(K key, V value) {
+        Entry<K, V> entry = new Entry<>(key, value, null);
+
+    }
+
+    class Entry<K, V> {
+        final K key;
+        V value;
+        Entry<K, V> next;
+
+        public Entry(K key, V value, Entry<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "Entry{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
+
+        public final int hashCode() {
+            return Objects.hashCode(key) ^ Objects.hashCode(value);
+        }
+
+        public final boolean equals(Object o) {
+            if (o == this)
+                return true;
+            if (o instanceof Map.Entry) {
+                Map.Entry<?,?> e = (Map.Entry<?,?>)o;
+                if (Objects.equals(key, e.getKey()) &&
+                        Objects.equals(value, e.getValue()))
+                    return true;
+            }
+            return false;
+        }
+    }
 }
