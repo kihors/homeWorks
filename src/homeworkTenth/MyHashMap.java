06b2bd5f1e6391c53ce2f64c828a4d1679116fc1
kihors <kihors@gmail.com>
2019-06-15 15:09:59 +0300

    homework number ten

--- src/homeworkTenth/MyHashMap.java
+++ src/homeworkTenth/MyHashMap.java
@@ -1,17 +1,70 @@
 package homeworkTenth;
 
-import java.util.Map;
-import java.util.Objects;
-
 public class MyHashMap<K, V> {
 
     private Entry<K, V>[] buckets;
     private static final int INITIAL_CAPACITY = 1 << 4;
     private int size = 0;
 
+    public MyHashMap() {
+        this(INITIAL_CAPACITY);
+    }
+
+    public MyHashMap(int capacity) {
+        this.buckets = new Entry[capacity];
+    }
+
     public void put(K key, V value) {
         Entry<K, V> entry = new Entry<>(key, value, null);
+        int bucket = getHash(key) % getBucketSize();
 
+        Entry<K, V> exiting = buckets[bucket];
+        if (exiting == null) {
+            buckets[bucket] = entry;
+            size++;
+        } else {
+            while (exiting.next != null) {
+                if (exiting.key.equals(key)) {
+                    exiting.value = value;
+                    return;
+                }
+                exiting = exiting.next;
+            }
+            if (exiting.key.equals(key)) {
+                exiting.value = value;
+            } else {
+                exiting.next = entry;
+                size++;
+            }
+        }
+    }
+
+    public V get(K key) {
+        Entry<K, V> bucket = buckets[getHash(key) % getBucketSize()];
+
+        while (bucket != null) {
+            if (key == bucket.key) {
+                return bucket.value;
+            }
+            bucket = bucket.next;
+        }
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private int getBucketSize() {
+        return buckets.length;
+    }
+
+    private int getHash(K key) {
+        if (key == null) {
+            return 0;
+        } else {
+            return Math.abs(key.hashCode());
+        }
     }
 
     class Entry<K, V> {
@@ -23,38 +76,6 @@ public class MyHashMap<K, V> {
             this.key = key;
             this.value = value;
             this.next = next;
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        @Override
-        public String toString() {
-            return "Entry{" +
-                    "key=" + key +
-                    ", value=" + value +
-                    '}';
-        }
-
-        public final int hashCode() {
-            return Objects.hashCode(key) ^ Objects.hashCode(value);
-        }
-
-        public final boolean equals(Object o) {
-            if (o == this)
-                return true;
-            if (o instanceof Map.Entry) {
-                Map.Entry<?,?> e = (Map.Entry<?,?>)o;
-                if (Objects.equals(key, e.getKey()) &&
-                        Objects.equals(value, e.getValue()))
-                    return true;
-            }
-            return false;
         }
     }
 }
---
+++ src/homeworkTenth/TestApp.java
@@ -0,0 +1,28 @@
+package homeworkTenth;
+
+public class TestApp {
+    public static void main(String[] args) {
+        MyHashMap<Integer, String > myHashMap = new MyHashMap<>();
+        myHashMap.put(1, "One");
+        myHashMap.put(2, "Two");
+        myHashMap.put(3, "Three");
+        myHashMap.put(4, "Four");
+        myHashMap.put(5, "Five");
+        myHashMap.put(6, "Six");
+        myHashMap.put(7, "Seven");
+        myHashMap.put(8, "Eight");
+        myHashMap.put(9, "Nine");
+        myHashMap.put(10, "Ten");
+
+        for (int i = 1; i < 100; i++) {
+            String value = "Key value is " + i;
+            myHashMap.put(i, value);
+        }
+
+        System.out.println(myHashMap.size());
+        System.out.println(myHashMap.get(5));
+        System.out.println(myHashMap.get(10));
+        System.out.println(myHashMap.get(20));
+        System.out.println(myHashMap.get(200));
+    }
+}
