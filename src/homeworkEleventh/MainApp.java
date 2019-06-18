518d7e599165de1c4a74a6a13512067bca4ec5eb
kihors <kihors@gmail.com>
2019-06-18 08:04:48 +0300

    added MainApp

---
+++ src/homeworkEleventh/MainApp.java
@@ -0,0 +1,24 @@
+package homeworkEleventh;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; scanner.hasNextInt(); i++) {
+            if (i % 2 != 0) {
+                arrayDeque.add(scanner.nextInt());
+            } else {
+                scanner.nextInt();
+            }
+        }
+        int size = arrayDeque.size();
+        for (int i = 0; i < size; i++) {
+            System.out.print(arrayDeque.removeLast() + " ");
+        }
+    }
+}
