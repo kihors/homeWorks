8783d73c7bc5ec52cc8cbc52a482e220106f7e57
kihors <kihors@gmail.com>
2019-06-21 09:23:19 +0300

    updated commit

---
+++ src/homeworkTwelfth/NumericalIntegration.java
@@ -0,0 +1,15 @@
+package homeworkTwelfth;
+
+import java.util.function.DoubleUnaryOperator;
+
+public class NumericalIntegration {
+    public static double integrate(DoubleUnaryOperator f, double a, double b) {
+        double result = 0;
+        double step = 1e-6;
+        while (a < b) {
+            result += step * f.applyAsDouble(a);
+            a += step;
+        }
+        return result;
+    }
+}
