08a0a6024da481537c5cc1066a666422f826baaa
kihors <kihors@gmail.com>
2019-06-21 08:47:52 +0300

    Initial commit

---
+++ src/homeworkTwelfth/TernaryOperator.java
@@ -0,0 +1,14 @@
+package homeworkTwelfth;
+
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class TernaryOperator {
+    private static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+        return t -> condition.test(t) ? ifTrue.apply(t) : ifFalse.apply(t);
+    }
+}
