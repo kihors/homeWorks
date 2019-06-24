4d0d85e83da112cd4193fde12ba2368655d24e41
kihors <kihors@gmail.com>
2019-06-24 15:45:15 +0300

    fixed some bugs

--- src/com/homework/fourteenth/MainApp.java
+++ src/com/homework/fourteenth/MainApp.java
@@ -3,6 +3,7 @@ package com.homework.fourteenth;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -15,10 +16,8 @@ public class MainApp {
                 .map(String::toLowerCase)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet().stream()
-                .sorted((a1, a2) -> {
-                    int res = a2.getValue().compareTo(a1.getValue());
-                    return res != 0 ? res : a1.getKey().compareTo(a2.getKey());
-                })
+                .sorted(Map.Entry.comparingByKey(Comparator.naturalOrder()))
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .limit(10)
                 .map(Map.Entry::getKey)
                 .forEach(System.out::println);
