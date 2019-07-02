d5897c1add5ef5bb28570ba6ecde4e8dfc6c65f9
kihors <kihors@gmail.com>
2019-07-02 16:11:57 +0300

    updated commit

--- src/com/homework/tenth/Map.java
+++ src/com/homework/tenth/Map.java
@@ -2,5 +2,10 @@ package com.homework.tenth;
 
 public interface Map<K, V> {
 
+    int size();
+    boolean isEmpty();
 
+    V get(Object key);
+    V put(K key, V value);
+    V remove(Object key);
 }
--- src/com/homework/tenth/MyMap.java
+++ src/com/homework/tenth/MyMap.java
@@ -6,5 +6,28 @@ public class MyMap<K, V> implements Map<K, V> {
 //    private Node<K, V>[] objects;
     private int size;
 
+    @Override
+    public int size() {
+        return 0;
+    }
 
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
 }
